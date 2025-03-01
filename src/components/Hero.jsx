089cf6039b4c0e5ecd5aef7d860ea54318117148
Trupti <truptivijayyadav27@gmail.com>
2025-03-02 00:13:01 +0530

    deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import TRUPTI from "../assets/trupti-2.png";
 import { Link } from "react-router-dom";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { RiContactsFill } from "react-icons/ri";
-import { MdDownload } from "react-icons/md";
+import { MdDownload, MdOpenInBrowser } from "react-icons/md";
 import { SectionWrapper } from "../hoc";
 const Hero = () => {
   return (
@@ -49,13 +49,14 @@ const Hero = () => {
               </button>
             </a>
 
-            <Link
+            <a
+              href="https://drive.google.com/file/d/1c95ydpLBNIVJxeWD-PMWn79pRaGLkiYU/view?usp=sharing"
               className="flex items-center gap-1 hover:gap-3 rounded-lg bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
               target="_blank">
-              <span>Get Resume</span>
-              <MdDownload size={16} />
-            </Link>
+              <span>View Resume</span>
+              <MdOpenInBrowser size={16} />
+            </a>
           </div>
         </div>
         <div className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
