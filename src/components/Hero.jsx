51ebf8895b671a67a66f7c2a9c2e67e53bac0820
Trupti <truptivijayyadav27@gmail.com>
2025-03-22 17:13:07 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import { MdDownload, MdOpenInBrowser } from "react-icons/md";
 import { SectionWrapper } from "../hoc";
 const Hero = () => {
   return (
-    <section className="relative w-full h-full sm:h-screen flex items-start md:mt-24 justify-center max-w-7xl mx-auto">
+    <section className="relative w-full h-full flex items-start md:mt-16 justify-center max-w-7xl mx-auto">
       <div className="grid grid-cols-1 items-center lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
